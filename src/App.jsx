3d27113483ca2489826c011db09d435294a3da44
Mshaxxaib <muhammad.shazaibb@gmail.com>
2026-03-18 11:09:01 +0400

    fixed base 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-const base = import.meta.env.BASE_URL;
 
 export default function App() {
+  const base = import.meta.env.BASE_URL;
   const projects = [
     {
       title: "VR Drone Simulator",
       description:
         "A VR training simulator with drone calibration, flight controls, and immersive interaction.",
-      image: '${base}projects/Drone.png',
+      image: `${base}projects/Drone.png`,
     },
     {
       title: "Crime Scene Simulation",
       description:
         "A realistic VR crime scene simulation for investigative training and scenario analysis.",
-      image: '${base}projects/CrimeScene.png',
+      image: `${base}projects/CrimeScene.png`,
     },
     {
       title: "Rowing Machine VR Experience",
       description:
         "An immersive VR experience using a real rowing machine for training and performance analysis.",
-      image: '${base}projects/Rowing.png',
+      image: `${base}projects/Rowing.png`,
     },
     {
       title: "First Aid Training Simulation",
       description:
         "An immersive VR experience for first aid training and emergency response simulation.",
-      image: '${base}projects/FirstAid.png',
+      image: `${base}projects/FirstAid.png`,
     },
     {
       title: "K9 Training Simulator",
       description:
         "An immersive VR experience for K9 training and simulation.",
-      image: '${base}projects/k9.png',
+      image: `${base}projects/k9.png`,
     },
     {
       title: "Arson Investigation Simulation",
       description:
         "An immersive VR experience for arson investigation and evidence analysis.",
-      image: '${base}projects/Arson.png',
+      image: `${base}projects/Arson.png`,
     },
     {
       title: "Driving Simulation",
       description:
         "An immersive VR experience for driving and traffic law training.",
-      image: '${base}projects/Driving.png',
+      image: `${base}projects/Driving.png`,
     },
   ];
 
@@ -347,14 +347,14 @@ function Hero3DCard() {
           transition={{ duration: 5, repeat: Infinity, ease: "easeInOut" }}
         >
           <img
-            src='${base}vr-headset.png'
+            src={`${base}vr-headset.png`}
             alt="VR Headset"
             className="h-full w-full object-contain drop-shadow-[0_0_40px_rgba(34,211,238,0.5)]"
           />
         </motion.div>
 
         <motion.img
-          src='${base}controller.png'
+          src={`${base}controller.png`}
           alt="Controller"
           className="absolute right-[-200px]  w-50 -translate-y-1/2 drop-shadow-[0_0_30px_rgba(34,211,238,0.35)]"
           animate={{ y: [0, -35, 0] }}
